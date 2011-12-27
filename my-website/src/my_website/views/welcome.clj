2f5502451c89e5737a80727629c7ca79e1af8eec
Rakhmad Azhari <rakhmad@csui01.org>
2011-12-27 22:29:53 +0700

    Initial Noir
    Website Template using Noir (http://webnoir.org)

---
+++ my-website/src/my_website/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns my-website.views.welcome
+  (:require [my-website.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to my-website"]))
